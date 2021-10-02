5737fa500cf4ed665fdfee9fc35a2878b19cb982
DavidMD1986 <ivory805@naver.com>
2021-10-02 20:15:34 +0900

    scripts fix

--- web/scripts/crawler.py
+++ web/scripts/crawler.py
@@ -5,7 +5,7 @@ from hotdeal.models import Deal
 from datetime import datetime, timedelta
 
 response = requests.get(
-    "https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu")
+    "https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu","https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu4","https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu8")
 
 soup = BeautifulSoup(response.text, "html.parser")
 BOT_TOKEN ="2048807117:AAE9eFTo-lRlm7HgkAzcCQ6D0R_Nw_4HYJI"
@@ -13,20 +13,19 @@ BOT_TOKEN ="2048807117:AAE9eFTo-lRlm7HgkAzcCQ6D0R_Nw_4HYJI"
 bot = telegram.Bot(token = BOT_TOKEN)
 
 def run():
-    #delete deals older than 3days
-
-    row, _ = Deal.objects.filter(created_at__lte=datetime.now() - timedelta(days=3)).delete()
+    # delete deals older than 3days
+    row, _ = Deal.objects.filter(created_at__lte=datetime.now() -
+                                 timedelta(days=3)).delete()
     print(row, "deals deleted")
-    
-    for item in soup.find_all("tr", {'class' : ["list1", "list0"]}):
+
+    for item in soup.find_all("tr", {'class': ["list1", "list0"]}):
         try:
             image = item.find("img", class_="thumb_border").get("src")[2:]
-            image = "http://" + image 
+            image = "http://" + image
             title = item.find("font", class_="list_title").text
             title = title.strip()
             link = item.find("font", class_="list_title").parent.get("href")
             link = "http://www.ppomppu.co.kr/zboard/" + link
-            
             reply_count = item.find("span", class_="list_comment2").text
             reply_count = int(reply_count)
             up_count = item.find_all("td")[-2].text
@@ -34,12 +33,11 @@ def run():
             up_count = int(up_count)
             if up_count >= 5:
                 if (Deal.objects.filter(link__iexact=link).count() == 0):
-                    Deal(image_url=image, title=title, link= link, reply_count=reply_count, up_count=up_count).save()
-                bot.sendMessage(-1001592964740, "{} {}".format(title, link))
+                    Deal(image_url=image, title=title, link=link,
+                         reply_count=reply_count, up_count=up_count).save()
+                    bot.sendMessage(-1001424017847,
+                                    '{} {}'.format(title, link))
 
         except Exception as e:
-        #print(e):
+            # print(e):
             continue
-
-
-
